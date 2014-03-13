4542a70f3a22c1a0a33fdbc7d3a76a6b4680300f
Jaccorot <caozijun007@163.com>
2014-03-14 01:17:51 +0800

    Day 2:Learn some methods

--- hello.py
+++ hello.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask,render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return "Hello World!"
+def index():
+	return "This is my python page."
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name = 1):
+	return render_template("hello.html",name=name)
+
+@app.route('/name/<username>')
+def say_hi(username):
+	return "It is %s,is that right?" % username
+
+@app.route('/number/<int:work_id>')
+def show_id(work_id):
+	return "Your number is %d,is that right?" % work_id
+
+
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug = True , host = '0.0.0.0')
