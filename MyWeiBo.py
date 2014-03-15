de63fc313988fa480de13ebf45ccd576f1563ab1
Jaccorot <caozijun007@163.com>
2014-03-15 11:36:43 +0800

    Day 3:Learn to make a website example

---
+++ MyWeiBo.py
@@ -0,0 +1,26 @@
+from __future__ import with_statement
+from contextlib import closing
+import sqlite3
+from flask import Flask, request, session, g,redirect, url_for, \
+	abort, render_template,flash
+
+DATABASE = '/tmp/MyWeiBo.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+	with closing(connect_db()) as db:
+		with app.open_resource('schema.sql') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
+
+if __name__ == '__main__':
+	app.run()
